31d512cb3a459c379e1fb52d5b0eb26ec72d6611
shefalimsr <201551095@iiitvadodara.ac.in>
2018-05-17 17:16:12 +0530

    first commit

---
+++ app/src/main/java/com/example/pay1/community/home/TrainingMaterial/RowView.java
@@ -0,0 +1,11 @@
+package com.example.pay1.community.home.TrainingMaterial;
+
+import android.graphics.Bitmap;
+
+public interface RowView {
+
+    void setTitle(String title);
+
+    void setIcon(Bitmap bitmap);
+
+}
